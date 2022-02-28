5693d49e1af609b16a3e86d09efdf4dee081b9a8
mcjovial <emmanuelelias455@gmail.com>
2022-03-01 00:39:00 +0100

    node & express setup

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.get("/", (req, res) => {
+    res.send("API is running..");
+});
+
+app.listen(PORT, console.log(`Server Started on PORT ${PORT}`));
