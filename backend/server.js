d4c170ca13cc386aaede54be0b4b866fc575188a
mcjovial <emmanuelelias455@gmail.com>
2022-03-01 19:04:35 +0100

    connect mongodb to nodejs

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const chats = require('./src/data/data');
+const mongoConnect = require('./src/services/mongo');
 
 dotenv.config();
+mongoConnect();
 const app = express();
 
 const PORT = process.env.PORT;
 
 app.get("/api/chat", (req, res) => {
-    res.send("API is running..");
+    res.send(chats);
 });
 
 app.listen(PORT, console.log(`Server Started on PORT ${PORT}`));
