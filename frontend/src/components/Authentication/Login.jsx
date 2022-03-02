12e8338ea0d43f604660575c84a8cee6fefb7583
mcjovial <emmanuelelias455@gmail.com>
2022-03-02 01:35:11 +0100

    frontend authentication

--- frontend/src/components/Authentication/Login.jsx
+++ frontend/src/components/Authentication/Login.jsx
@@ -1,58 +1,121 @@
 import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, VStack } from '@chakra-ui/react'
 import React, { useState } from 'react'
+import axios from "axios";
+import { useToast } from "@chakra-ui/react";
+import { useHistory } from "react-router-dom";
 
 
 const Login = () => {
-const [email, setEmail] = useState('')
-const [password, setPassword] = useState('')
-const [show, setShow] = useState(false)
-const [loading, setLoading] = useState(false)
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [show, setShow] = useState(false)
+    const [loading, setLoading] = useState(false)
+    const toast = useToast();
 
 
-return (
-    <VStack spacing="10px">
-        <FormControl id='email' isRequired>
-            <FormLabel>Email Address</FormLabel>
-            <Input 
-                value={email}
-                type='email'
-                placeholder='Enter Your Email Address'
-                onChange={(e) => setEmail(e.target.value)}
-            />
-        </FormControl>
-        <FormControl id='password' isRequired>
-            <FormLabel>Password</FormLabel>
-            <InputGroup size='md'>
+    const history = useHistory();
+
+    const submitHandler = async () => {
+        setLoading(true);
+        if (!email || !password) {
+            toast({
+                title: "Please Fill all the Feilds",
+                status: "warning",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            setLoading(false);
+            return;
+        }
+
+        // console.log(email, password);
+        try {
+            const config = {
+                headers: {
+                "Content-type": "application/json",
+                },
+            };
+
+            const { data } = await axios.post(
+                "/v1/api/users/login",
+                { email, password },
+                config
+            );
+
+            // console.log(JSON.stringify(data));
+            toast({
+                title: "Login Successful",
+                status: "success",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            localStorage.setItem("userInfo", JSON.stringify(data));
+            setLoading(false);
+            history.push("/chats");
+        } catch (error) {
+            toast({
+                title: "Error Occured!",
+                description: error.response.data.message,
+                status: "error",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            setLoading(false);
+        }
+    };
+
+    return (
+        <VStack spacing="10px">
+            <FormControl id='email' isRequired>
+                <FormLabel>Email Address</FormLabel>
                 <Input 
-                    value={password}
-                    type={show ? 'text' : 'password'}
-                    placeholder='Enter Your Password'
-                    onChange={(e) => setPassword(e.target.value)}
+                    value={email}
+                    type='email'
+                    placeholder='Enter Your Email Address'
+                    onChange={(e) => setEmail(e.target.value)}
                 />
-                <InputRightElement width='4.5rem'>
-                    <Button h='1.75rem' size='sm' onClick={(e) => setShow(!show)}>
-                        {show? 'Hide' : 'Show'}
-                    </Button>
-                </InputRightElement>
-            </InputGroup>
-        </FormControl>
-        <Button 
-            colorScheme='blue'
-            width='100%'
-            style={{marginTop: 15}}
-            isLoading={loading}
-        >
-            Login
-        </Button>
-        <Button
-            variant='solid'
-            colorScheme='red'
-            width='100%'
-        >
-            Guest User
-        </Button>
-    </VStack>
-  )
+            </FormControl>
+            <FormControl id='password' isRequired>
+                <FormLabel>Password</FormLabel>
+                <InputGroup size='md'>
+                    <Input 
+                        value={password}
+                        type={show ? 'text' : 'password'}
+                        placeholder='Enter Your Password'
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <InputRightElement width='4.5rem'>
+                        <Button h='1.75rem' size='sm' onClick={(e) => setShow(!show)}>
+                            {show? 'Hide' : 'Show'}
+                        </Button>
+                    </InputRightElement>
+                </InputGroup>
+            </FormControl>
+            <Button 
+                colorScheme='blue'
+                width='100%'
+                style={{marginTop: 15}}
+                onClick={submitHandler}
+                isLoading={loading}
+            >
+                Login
+            </Button>
+            <Button
+                variant='solid'
+                colorScheme='red'
+                width='100%'
+                onClick={() => {
+                    setEmail("emmanuel@email.com");
+                    setPassword("password");
+                }}
+            >
+                Guest User
+            </Button>
+        </VStack>
+    )
 }
 
 export default Login
