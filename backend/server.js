dac1f91ba46ae8a95b6a759b29ca16ca50abe51b
mcjovial <emmanuelelias455@gmail.com>
2022-03-17 10:14:50 +0100

    socket server for chats

--- backend/server.js
+++ backend/server.js
@@ -17,8 +17,48 @@ app.use('/v1/api', api);
 
 app.use(notFound);
 app.use(errorHandler)
-app.get("/api/chat", (req, res) => {
-    res.send(chats);
+
+app.get("/", (req, res) => {
+    res.send('v1 API is running..');
 });
 
-app.listen(PORT, console.log(`Server Started on PORT ${PORT}`));
+const server = app.listen(PORT, console.log(`Server Started on PORT ${PORT}...`));
+
+const io = require('socket.io')(server, {
+    pingTimeout: 60000,
+    cors: {
+        origin: 'http://localhost:3000',
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log("Connected to socket.io");
+    socket.on("setup", (userData) => {
+      socket.join(userData._id);
+      socket.emit("connected");
+    });
+  
+    socket.on("join chat", (room) => {
+      socket.join(room);
+      console.log("User Joined Room: " + room);
+    });
+    socket.on("typing", (room) => socket.in(room).emit("typing"));
+    socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+  
+    socket.on("new message", (newMessageRecieved) => {
+      var chat = newMessageRecieved.chat;
+  
+      if (!chat.users) return console.log("chat.users not defined");
+  
+      chat.users.forEach((user) => {
+        if (user._id == newMessageRecieved.sender._id) return;
+  
+        socket.in(user._id).emit("message recieved", newMessageRecieved);
+      });
+    });
+  
+    socket.off("setup", () => {
+      console.log("USER DISCONNECTED");
+      socket.leave(userData._id);
+    });
+  });
